c2d774743b1d06ad7ba6cf5723ba538cb173d9b8
Manuel Aguilera <102045443+aguileraDev@users.noreply.github.com>
2024-09-18 18:18:24 -0400

    feat: navbar component created

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,20 @@
 <script setup lang="ts">
+import { ref } from 'vue';
+import NavComponent from '@/components/NavComponent.vue';
+
+const project = ref(import.meta.env.VITE_APP_TITLE);
+
+const navItems = ref([
+{ text: 'Services', href: '#services' },
+{ text: 'Products', href: '#products' },
+{ text: 'Our Brand', href: '#brand' },
+{ text: 'Contact', href: '#contact' }
+])
 
 </script>
 
 <template>
-  <h1 class="text-3xl font-bold underline">
-    Hello world!
-  </h1>
+  <NavComponent :project="project" :nav-items="navItems"/>
 </template>
 
 <style scoped>
---
+++ src/components/NavComponent.vue
@@ -0,0 +1,30 @@
+<script setup lang="ts">
+import { defineProps, type PropType } from 'vue';
+
+const props = defineProps({
+    navItems: {
+        type: Array as PropType<{ text: string; href: string }[]>,
+        required: true
+    },
+    project: {
+        type: String,
+        required: true,
+    }
+});
+
+
+</script>
+
+<template>
+    <nav class="bg-white shadow-sm">
+        <div class="container mx-auto p-4 flex justify-between items-center">
+            <a href="#" class="text-2xl font-bold text-blue-600">{{ project.toUpperCase() }}</a>
+            <div class="space-x-4">
+                <a v-for="item in navItems" :key="item.href" :href="item.href"
+                    class="text-gray-600 hover:text-blue-600 transition-colors">{{ item.text }}</a>
+            </div>
+        </div>
+    </nav>
+</template>
+
+<style scoped></style>
